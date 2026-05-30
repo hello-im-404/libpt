ec00e50887e6a1c8584f3b896d57dabb0d30b03a
hello-im-404 <202427842+hello-im-404@users.noreply.github.com>
2026-05-30 19:11:06 +0300

    asdad

--- compile.sh
+++ compile.sh
@@ -9,4 +9,6 @@ if [[ "$choice" =~ ^[yY]$ ]]; then
     sudo make install
 else
     echo "[+] Local build is complete in the 'out/' folder."
+	sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/local.conf'
+	sudo ldconfig
 fi
